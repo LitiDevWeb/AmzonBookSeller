7692c56873009057f35acad570ee994a8b560705
xlinca25 <97301279+LitiDevWeb@users.noreply.github.com>
2023-08-26 20:56:46 +0100

    Add files via upload

---
+++ src/books.js
@@ -0,0 +1,34 @@
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
+import img4 from "./images/book-4.jpg";
+
+export const books = [
+  {
+    title: "One Last Rainy Day: The Legacy ",
+    author: "Kate Stewart",
+    img: img1,
+    id: 1,
+  },
+
+  {
+    title: "It Ends with Us: A Novel",
+    author: "Colleen Hoover",
+    img: img2,
+    id: 2,
+  },
+
+  {
+    title: "Killers of the Flower Moon ",
+    author: "David Grann ",
+    img: img3,
+    id: 3,
+  },
+
+  {
+    title: " Atomic Habits: An Easy & Proven Way to Build Good Habits",
+    author: "James Clear ",
+    img: img4,
+    id: 4,
+  },
+];
